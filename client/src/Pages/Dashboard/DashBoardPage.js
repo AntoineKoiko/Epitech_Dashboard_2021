2946e82fa61653b35ccd2fe099570af1bc1ff0ba
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-11 17:25:03 +0100

    feat(Pages/Dashboard): create basic page to add it in the routes

---
+++ client/src/Pages/Dashboard/DashBoardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DefaultHeader from '../../Compontents/DefaultHeader/DefaultHeader';
+
+function DashboardPage() {
+    return (
+        <div>
+            <DefaultHeader />
+            <p>I'm the dasboard page</p>
+        </div>
+    )
+}
+
+export default DashboardPage;
