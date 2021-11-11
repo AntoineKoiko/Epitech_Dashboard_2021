66c05a05c5a618c5fcea3d9b1219e7b9c59ba038
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-11 17:24:21 +0100

    feat(DefautlHeader): basic hearder with title

---
+++ client/src/Compontents/DefaultHeader/DefaultHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DefaultHeader.css';
+
+function DefaultHeader() {
+    return (
+        <div class="header">
+            <h3>Dashboard</h3>
+        </div>
+    )
+}
+
+export default DefaultHeader;
