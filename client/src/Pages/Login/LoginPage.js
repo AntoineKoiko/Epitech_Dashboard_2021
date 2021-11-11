9a146b957bc86d5af76a28c01d7cbe703ade1181
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-11 18:01:34 +0100

    feat(Pages): add Default header

--- client/src/Pages/Login/LoginPage.js
+++ client/src/Pages/Login/LoginPage.js
@@ -4,6 +4,9 @@ import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 import { createTheme } from '@mui/system';
 import './LoginPage.css';
+import '../../Styles/GlobalStyle.css';
+import DefaultHeader from '../../Compontents/DefaultHeader/DefaultHeader';
+
 
 const responseGoogle = (response) => {
     console.log(response);
@@ -11,47 +14,50 @@ const responseGoogle = (response) => {
 
 function LoginPage() {
     return (
-        <div className="page-container">
-            <div className="main-container">
-                <p>Pour continuer, connectez-vous a Dashboard</p>
-                <Button sx={theme.inputField} className="input-field" href="https://www.spotify.com" variant="contained">Continuer avec spotifyd</Button>
-                <Button sx={theme.inputField} className="input-field" href="https://www.reddit.com" variant="contained">Continuer avec reddit</Button>
-                <GoogleLogin
-                    className="input-field"
-                    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                    buttonText="Continuer avec Google"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                />
+        <div>
+            <DefaultHeader/>
+            <div className="center-h">
+                <div className="main-container">
+                    <p>Pour continuer, connectez-vous a Dashboard</p>
+                    <Button sx={theme.inputField} className="input-field" href="https://www.spotify.com" variant="contained">Continuer avec spotifyd</Button>
+                    <Button sx={theme.inputField} className="input-field" href="https://www.reddit.com" variant="contained">Continuer avec reddit</Button>
+                    <GoogleLogin
+                        className="input-field"
+                        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                        buttonText="Continuer avec Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
 
-                <h2><span>ou</span></h2>
+                    <h2><span>ou</span></h2>
 
-                <p>Continuer avec vos identifiants:</p>
-                <TextField
-                sx={theme.inputField}
-                required
-                id="login-name"
-                label="Email / Pseudo"
-                variant="outlined"
-                placeholder="John.doe@email.com"
-                />
-                <TextField
-                sx={theme.inputField}
-                required
-                id="login-name"
-                label="Mot de passe"
-                variant="outlined"
-                type="password"
-                autoComplete="current-password"
-                />
-                <Button sx={theme.inputField} variant="contained">Se Connecter</Button>
+                    <p>Continuer avec vos identifiants:</p>
+                    <TextField
+                    sx={theme.inputField}
+                    required
+                    id="login-name"
+                    label="Email / Pseudo"
+                    variant="outlined"
+                    placeholder="John.doe@email.com"
+                    />
+                    <TextField
+                    sx={theme.inputField}
+                    required
+                    id="login-name"
+                    label="Mot de passe"
+                    variant="outlined"
+                    type="password"
+                    autoComplete="current-password"
+                    />
+                    <Button sx={theme.inputField} variant="contained">Se Connecter</Button>
 
-                <h2><span>Pas encore inscrit ?</span></h2>
+                    <h2><span>Pas encore inscrit ?</span></h2>
 
-                <Button href="/register" sx={theme.inputField} variant="contained">
-                    S'inscire
-                </Button>
+                    <Button href="/register" sx={theme.inputField} variant="contained">
+                        S'inscire
+                    </Button>
+                </div>
             </div>
         </div>
     )
--- client/src/Pages/Register/RegisterPage.js
+++ client/src/Pages/Register/RegisterPage.js
@@ -4,6 +4,8 @@ import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 import { createTheme } from '@mui/system';
 import './RegisterPage.css';
+import '../../Styles/GlobalStyle.css';
+import DefaultHeader from '../../Compontents/DefaultHeader/DefaultHeader';
 
 const responseGoogle = (response) => {
     console.log(response);
@@ -11,67 +13,70 @@ const responseGoogle = (response) => {
 
 function RegisterPage() {
     return (
-        <div className="page-container">
-            <div className="main-container">
-                <p>Pour continuer, inscrivez-vous a Dashboard</p>
+        <div>
+            <DefaultHeader />
+            <div className="center-h">
+                <div className="main-container">
+                    <p>Pour continuer, inscrivez-vous a Dashboard</p>
 
-                <Button sx={theme.inputField} className="input-field" href="https://www.spotify.com" variant="contained">Inscrivez-vous avec spotifyd</Button>
-                <Button sx={theme.inputField} className="input-field" href="https://www.reddit.com" variant="contained">Inscrivez-vous avec reddit</Button>
-                <GoogleLogin
-                    className="input-field"
-                    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                    buttonText="Inscrivez-vous avec Google"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                />
+                    <Button sx={theme.inputField} className="input-field" href="https://www.spotify.com" variant="contained">Inscrivez-vous avec spotifyd</Button>
+                    <Button sx={theme.inputField} className="input-field" href="https://www.reddit.com" variant="contained">Inscrivez-vous avec reddit</Button>
+                    <GoogleLogin
+                        className="input-field"
+                        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                        buttonText="Inscrivez-vous avec Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
 
-                <h2><span>ou</span></h2>
+                    <h2><span>ou</span></h2>
 
-                <p>Inscrivez-vous avec votre email:</p>
-                <TextField
-                sx={theme.inputField}
-                required
-                id="register-mail"
-                label="Quel est votre email ?"
-                variant="outlined"
-                placeholder="John.doe@email.com"
-                />
-                <TextField
-                sx={theme.inputField}
-                required
-                id="register-mail-conf"
-                label="Confirmez votre email ?"
-                variant="outlined"
-                placeholder="John.doe@email.com"
-                />
+                    <p>Inscrivez-vous avec votre email:</p>
+                    <TextField
+                    sx={theme.inputField}
+                    required
+                    id="register-mail"
+                    label="Quel est votre email ?"
+                    variant="outlined"
+                    placeholder="John.doe@email.com"
+                    />
+                    <TextField
+                    sx={theme.inputField}
+                    required
+                    id="register-mail-conf"
+                    label="Confirmez votre email ?"
+                    variant="outlined"
+                    placeholder="John.doe@email.com"
+                    />
 
-                <TextField
-                sx={theme.inputField}
-                required
-                id="register-password"
-                label="Quel est votre mot de passe ?"
-                variant="outlined"
-                type="password"
-                autoComplete="current-password"
-                />
-                <TextField
-                sx={theme.inputField}
-                required
-                id="register-password-conf"
-                label="Confirmez votre mot de passe ?"
-                variant="outlined"
-                type="password"
-                autoComplete="current-password"
-                />
+                    <TextField
+                    sx={theme.inputField}
+                    required
+                    id="register-password"
+                    label="Quel est votre mot de passe ?"
+                    variant="outlined"
+                    type="password"
+                    autoComplete="current-password"
+                    />
+                    <TextField
+                    sx={theme.inputField}
+                    required
+                    id="register-password-conf"
+                    label="Confirmez votre mot de passe ?"
+                    variant="outlined"
+                    type="password"
+                    autoComplete="current-password"
+                    />
 
-                <Button sx={theme.inputField} variant="contained">S'inscrire</Button>
+                    <Button sx={theme.inputField} variant="contained">S'inscrire</Button>
 
-                <h2><span>Déja un compte ?</span></h2>
+                    <h2><span>Déja un compte ?</span></h2>
 
-                <Button href="/login" sx={theme.inputField} variant="contained">
-                    Se connecter
-                </Button>
+                    <Button href="/login" sx={theme.inputField} variant="contained">
+                        Se connecter
+                    </Button>
+                </div>
             </div>
         </div>
     )
