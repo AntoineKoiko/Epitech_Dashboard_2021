2e836c150412cc821c233836ba2168d0afd056f0
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-15 22:46:36 +0100

    feat(StockWidget): customize component and set values as variable to prepare API call

--- client/src/Components/StockWidget/StockWindget.js
+++ client/src/Components/StockWidget/StockWindget.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './StockWidget.css';
 import WidgetFrame from '../WidgetFrame';
+import { useState } from 'react';
 
-function StockWidget() {
+
+function StockWidget(props) {
+    const [value, setValue] =  useState(100);
+    const [evolution, setEvolution] = useState(1.05);
+    const stockName = props.name ? props.name : 'AAPL';
+
+
     return (
-        <WidgetFrame title="Stock" subtitle="AAPL">
-            <h1>Value: 100$  +1%</h1>
+        <WidgetFrame title="Stock" subtitle={stockName}>
+            <div className="stock-widget">
+                <p>
+                    Value: {value}$
+                      <span style={{color: evolution < 0 ? '#F00' : '#0F0'}}>
+                          {evolution < 0 ? '-' : '+'}{evolution}%
+                      </span>
+                </p>
+            </div>
         </WidgetFrame>
     )
 }
