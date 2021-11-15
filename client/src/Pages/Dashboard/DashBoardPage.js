064a08c260a27ac738de85ad94045aabe824c5ef
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-15 21:46:04 +0100

    feat(DashboardPage): display stock widget

--- client/src/Pages/Dashboard/DashBoardPage.js
+++ client/src/Pages/Dashboard/DashBoardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import StockWidget from '../../Components/StockWidget';
 
 function DashboardPage() {
     return (
         <div>
             <p>I'm the dasboard page</p>
+            <StockWidget/>
         </div>
     )
 }
