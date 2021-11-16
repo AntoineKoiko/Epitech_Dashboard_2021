dbf2a844f028f44ca39d23fb1ef6e229161fc1ac
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-16 15:04:01 +0100

    feat(DashboardPage): add RenderWeatherwidget

--- client/src/Pages/Dashboard/DashBoardPage.js
+++ client/src/Pages/Dashboard/DashBoardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import RenderStockWidget from '../../Components/StockWidget/RenderStockWidget';
+import WeatherWidget from '../../Components/WeatherWidget/';
 
 function DashboardPage() {
     return (
         <div>
             <p>I'm the dasboard page</p>
             <RenderStockWidget stockID="AAPL"/>
+            <WeatherWidget cityID="Rennes"/>
         </div>
     )
 }
