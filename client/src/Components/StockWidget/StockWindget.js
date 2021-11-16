69bbddc7e26980f4c7657c3d369acadeff7bca40
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-16 13:37:46 +0100

    feat(StockWidget): make a dump and logic component and display all information about stock

--- client/src/Components/StockWidget/StockWindget.js
+++ client/src/Components/StockWidget/StockWindget.js
@@ -3,60 +3,37 @@ import './StockWidget.css';
 import WidgetFrame from '../WidgetFrame';
 import { useState, useEffect } from 'react';
 
-const requestOptions = {
-    method: "GET",
-    credentials: "include",
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Credentials": true
-    }
+function StockDetail ({stockInfo}) {
+    return (
+        <div className="stock-widget">
+            <p>
+                lowestPrice: {stockInfo.lowestPrice} / Highest price: {stockInfo.highestPrice}
+            </p>
+            <p>
+                Open price: {stockInfo.openPrice}
+            </p>
+            <p>
+                Last close price: {stockInfo.prevClosePrice}
+            </p>
+        </div>
+    )
 }
 
-function StockWidget({stockID}) {
-    const stockName = stockID ? stockID : 'MSFT';
-    const [stockInfo, setStockInfo] = useState({
-        'price': 0,
-        'percentChange': 0,
-        'valueChange': 0,
-    });
-
-    useEffect(() => {
-        fetch('http://localhost:8080/stock?name=' + stockName, requestOptions)
-        .then(response => {
-            if (response.status === 200)
-                return response.json();
-            throw new Error("failed to authenticate user")
-        })
-        .then(responseJSON => {
-            console.log('json response', responseJSON);
-            setStockInfo({
-                'price': responseJSON.currentPrice,
-                'percentChange': responseJSON.percentChange,
-                'valueChange': responseJSON.change,
-            })
-        })
-        .catch(error => {
-            console.log('fetch error');
-            // setAutenticated(false);
-            // setError("Failed to authenticate user");
-        })
-
-    }, []);
-
+function StockWidget({stockID, stockInfo}) {
+    const expandContent = <StockDetail stockInfo={stockInfo} />;
 
     return (
-        <WidgetFrame title="Stock" subtitle={stockName}>
+        <WidgetFrame title="Stock" subtitle={stockID} expand={true} expandContent={expandContent}>
             <div className="stock-widget">
                 <p>
-                    Value: {stockInfo.price}$
+                    Value: {stockInfo.currentPrice}$
                       <span style={{color: stockInfo.percentChange < 0 ? '#F00' : '#0F0'}}>
                           {stockInfo.percentChange < 0 ? '-' : '+'}{stockInfo.percentChange}%
                       </span>
                 </p>
                 <p>
                     <span style={{color: stockInfo.valueChange < 0 ? '#F00' : '#0F0'}}>
-                          {stockInfo.valueChange < 0 ? '-' : '+'}{stockInfo.valueChange}$
+                          {stockInfo.change < 0 ? '-' : '+'}{stockInfo.change}$
                       </span>
                 </p>
             </div>
--- client/src/Components/StockWidget/index.js
+++ client/src/Components/StockWidget/index.js
@@ -1,1 +1,1 @@
-export {default} from './StockWindget';
+export {default} from './RenderStockWidget';
