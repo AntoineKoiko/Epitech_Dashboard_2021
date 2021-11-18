cd3ce42ad6fac801f73ed60c12cc6c5a3b8f6897
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-18 14:34:43 +0100

    feat(AddWidgetModal): set up form to select new widget

--- client/src/Components/AddWidgetModal/AddWidgetModal.js
+++ client/src/Components/AddWidgetModal/AddWidgetModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -9,15 +9,62 @@ import DialogTitle from '@mui/material/DialogTitle';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 
-import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+
+const FormItems = [
+    {
+        id: 'youtube',
+        label: 'Youtube',
+        widgets: [
+            {
+                id: 'comment',
+                label: 'Comment',
+                optionLabel: 'Choose a video',
+            },
+            {
+                id: 'view',
+                label: 'View',
+                optionLabel: 'Choose a video',
+            },
+        ],
+    },
+
+    {
+        id: 'spotify',
+        label: 'Spotify',
+        widgets: [
+            {
+                id: 'trackts',
+                label: 'Tracks',
+                optionLabel: 'Choose artist / time'
+            },
+        ],
+    },
+];
 
 
 function AddWidgetModal ({handler, open}) {
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const [serviceSelect, setService] = useState(FormItems[0]);
+    const [widgetSelect, setWidget] = useState(serviceSelect.widgets[0]);
 
-    const handleClickOpen = () => {
-      handler(true);
+    const ChangeService = (event) => {
+        const newID = event.target.value;
+        const newSelected = FormItems.find(elt => elt.id === newID);
+        setService(newSelected);
+        setWidget(newSelected.widgets[0]);
+    };
+
+    const ChangeWidget = (event) => {
+        const newID = event.target.value;
+        const newSelected = serviceSelect.widgets.find(elt => elt.id === newID);
+        setWidget(newSelected);
     };
 
     const handleClose = () => {
@@ -34,13 +81,47 @@ function AddWidgetModal ({handler, open}) {
           aria-labelledby="responsive-dialog-title"
         >
           <DialogTitle id="responsive-dialog-title">
-            {"Use Google's location service?"}
+            {"Which widget do you want to add ? "}
           </DialogTitle>
           <DialogContent>
             <DialogContentText>
-              Let Google help apps determine location. This means sending anonymous
-              location data to Google, even when no apps are running.
+              Choose a service:
             </DialogContentText>
+
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Service</FormLabel>
+              <RadioGroup
+                row aria-label="service"
+                name="row-radio-buttons-group"
+                onChange={ChangeService}
+                value={serviceSelect.id}
+            >
+                {FormItems.map((service) => {
+                    return (
+                        <FormControlLabel value={service.id} control={<Radio />} label={service.label} />
+                    )
+                })}
+
+              </RadioGroup>
+
+              <FormLabel component="legend">Widget</FormLabel>
+              <RadioGroup
+              row aria-label="widget"
+              name="row-radio-buttons-group"
+              onChange={ChangeWidget}
+              value={widgetSelect.id}
+              >
+                {serviceSelect.widgets.map((widget) => {
+                    return (
+                        <FormControlLabel value={widget.id} control={<Radio />} label={widget.label} />
+                    )
+                })}
+
+              </RadioGroup>
+              <TextField id="opt" label={widgetSelect.optionLabel} variant="standard" />
+            </FormControl>
+
+
           </DialogContent>
           <DialogActions>
             <Button autoFocus onClick={handleClose}>
