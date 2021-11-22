b191427b7bccef36f5778e6d151e8482f60f61f4
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-11-22 11:42:20 +0100

    feat(server/routes/authRoutes.js) send 401 for /login/sucess if user not auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -15,6 +15,7 @@ router.get("/login/success", (req, res) => {
             cookies: req.cookies
         });
     } else {
+        res.send(401).status("User is not Login");
         console.log("user null")
     }
 })
