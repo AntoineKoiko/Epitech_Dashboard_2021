6bfd91b4f38272192a5c87f4a765a613a5ee1e0c
AntoineKoiko <antoine.couacault@epitech.eu>
2021-11-22 23:21:53 +0100

    feat(DashboardPage): make widget dragable

--- client/src/Pages/Dashboard/DashBoardPage.js
+++ client/src/Pages/Dashboard/DashBoardPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RenderStockWidget from '../../Components/Widgets/StockWidget/RenderStockWidget';
 import WeatherWidget from '../../Components/Widgets/WeatherWidget/';
 import YoutubeCommentWidget from '../../Components/Widgets/YoutubeCommentWidget';
@@ -6,16 +6,38 @@ import YoutubeSubNBWidget from '../../Components/Widgets/YoutubeSubNBWidget';
 import RedditSubFeedWidget from '../../Components/Widgets/RedditSubFeedWidget';
 import SpotifyTopWidget from '../../Components/Widgets/SpotifyTopWidget/SpotifyTopWidget';
 
+//Drag
+import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
+
+function ToDrag({x, y, children}) {
+    return (
+        <Draggable
+            axis="both"
+            handle=".handle"
+            defaultPosition={{x: x, y: y}}
+            position={null}
+            grid={[25, 25]}
+            scale={1}
+        >
+            <div className="handle">
+                {children}
+            </div>
+        </Draggable>
+    );
+}
+
+
 function DashboardPage() {
     return (
         <div>
             <p>I'm the dasboard page</p>
-            <RenderStockWidget stockID="AAPL"/>
-            <WeatherWidget cityID="Rennes"/>
-            <YoutubeCommentWidget videoTitle="test"/>
-            <YoutubeSubNBWidget/>
-            <RedditSubFeedWidget/>
-            <SpotifyTopWidget/>
+
+            <ToDrag x={0} y={0}><RenderStockWidget stockID="AAPL"/></ToDrag>
+            <ToDrag x={0} y={0}><WeatherWidget cityID="Rennes"/></ToDrag>
+            <ToDrag x={0} y={0}><YoutubeCommentWidget videoTitle="test"/></ToDrag>
+            <ToDrag x={0} y={0}><YoutubeSubNBWidget/></ToDrag>
+            <ToDrag x={0} y={0}><RedditSubFeedWidget/></ToDrag>
+            <ToDrag x={0} y={0}><SpotifyTopWidget/></ToDrag>
         </div>
     )
 }
