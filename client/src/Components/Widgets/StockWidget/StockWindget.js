d9e222f54c7a016450941ae032ec3b565b426729
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-11-25 23:48:39 +0100

    feat(StockWidget.js) better style for widget

--- client/src/Components/Widgets/StockWidget/StockWindget.js
+++ client/src/Components/Widgets/StockWidget/StockWindget.js
@@ -27,17 +27,25 @@ function StockWidget({stockID, stockInfo}) {
     return (
         <WidgetFrame title="Stock" subtitle={stockID} expand={true} expandContent={expandContent}>
             <div className="stock-widget">
-                <p>
+                <h2>${stockInfo.currentPrice}</h2>
+                <h4 style={{color: stockInfo.percentChange < 0 ? '#D0312D' : '#3CB043', fontWeight: "bold" }}>
+                    {stockInfo.change < 0 ? '' : '+'}{stockInfo.change} ({Math.round(stockInfo.percentChange * 100) / 100} %) aujourd'hui 
+                </h4>
+                <h5>Ouverture {stockInfo.openPrice}</h5>
+                <h5>+Haut {stockInfo.highestPrice}</h5>
+                <h5>+Bas {stockInfo.lowestPrice}</h5>
+                <h5>Cours de clôture {stockInfo.prevClosePrice}</h5>
+                {/* <p>
                     Value: {stockInfo.currentPrice}$
                     <span style={{color: stockInfo.percentChange < 0 ? '#F00' : '#0F0'}}>
                         {stockInfo.percentChange < 0 ? '-' : '+'}{stockInfo.percentChange}%
                     </span>
                 </p>
                 <p>
-                    <span style={{color: stockInfo.valueChange < 0 ? '#F00' : '#0F0'}}>
+                    <span style={{color: stockInfo.change < 0 ? '#F00' : '#0F0'}}>
                         {stockInfo.change < 0 ? '-' : '+'}{stockInfo.change}$
                     </span>
-                </p>
+                </p> */}
             </div>
         </WidgetFrame>
     )
