cf2749d8d0b4b8b3f064fba8e194ca872986ffd6
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-12-01 22:45:11 +0100

    feat(YoutubeSubNBWidget.js) replace G by Md for Milliards

--- client/src/Components/Widgets/YoutubeSubNBWidget/YoutubeSubNBWidget.js
+++ client/src/Components/Widgets/YoutubeSubNBWidget/YoutubeSubNBWidget.js
@@ -8,7 +8,7 @@ function nbFormatter(num, digits) {
         { value: 1, symbol: "" },
         { value: 1e3, symbol: "k" },
         { value: 1e6, symbol: "M" },
-        { value: 1e9, symbol: "G" },
+        { value: 1e9, symbol: "Md" },
         { value: 1e12, symbol: "T" },
         { value: 1e15, symbol: "P" },
         { value: 1e18, symbol: "E" }
