3f77ab551bce171f61a0d6d3743cd107a6bc7947
AntoineKoiko <antoine.couacault@epitech.eu>
2021-12-02 17:48:33 +0100

    feat(client/scr/SettingModal): improve design

--- client/src/Components/SettingModal/SettingModal.js
+++ client/src/Components/SettingModal/SettingModal.js
@@ -5,34 +5,33 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 
+import './SettingModal.css';
 
 function SettingModal ({open, openHandler}) {
     return (
-        <div>
-            <Dialog
-                open={open}
-                onClose={() => openHandler(false)}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {"Connect with other service ?"}
-                </DialogTitle>
-                <DialogContent>
-
-
+        <Dialog
+            open={open}
+            onClose={() => openHandler(false)}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                {"Connect with other service ?"}
+            </DialogTitle>
+            <DialogContent>
+                <div className="btn-container">
                     <Button sx={{width: '60%', marginTop: 2}} className="input-field" href="http://localhost:8080/auth/spotify" variant="contained">Continuer avec spotifyd</Button>
                     <Button sx={{width: '60%', marginTop: 2}} className="input-field" href="http://localhost:8080/auth/reddit" variant="contained">Continuer avec reddit</Button>
                     <Button sx={{width: '60%', marginTop: 2}} className="input-field" href="http://localhost:8080/auth/google" variant="contained">Continuer avec google</Button>
-                </DialogContent>
+                </div>
+            </DialogContent>
 
-                <DialogActions>
-                    <Button onClick={() => openHandler(false)}>
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
+            <DialogActions>
+                <Button onClick={() => openHandler(false)}>
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
     )
 }
 
