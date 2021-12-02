f0446f155db7a842b3db3e8a5821b078e2255366
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-12-02 18:06:18 +0100

    feat(server/services/weatherService.js) add searchLocation service

--- server/services/weatherService.js
+++ server/services/weatherService.js
@@ -12,10 +12,28 @@ const getWeather = async (cityName) => {
         const result = await axios.get(weatherURL.href);
         return result.data;
     } catch (error) {
-        throw `getWeather ${error.toString()}`
+        throw `getWeather ${error.toString()}`;
+    }
+}
+
+const searchLocation = async(query) => {
+    const searchURL = new URL(`${weatherConfig.WEATHER_API_URI}search.json`);
+
+    searchURL.searchParams.append("key", weatherConfig.WEATHER_API_KEY);
+    searchURL.searchParams.append("q", query);
+
+    try {
+        const result = await axios.get(searchURL.href);
+        const resultArray = result.data.map(element => {
+            return {label: element.name}
+        })
+        return resultArray;
+    } catch (error) {
+        throw `searchLocation ${error.toString()}`;
     }
 }
 
 module.exports = {
-    getWeather
+    getWeather,
+    searchLocation
 }
