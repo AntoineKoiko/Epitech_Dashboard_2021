ff371f5cecd5053f1feb35424b261f7c17da2ed6
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-12-04 18:25:42 +0100

    fix(client/src/Components/WeatherCity) using useCallback

--- client/src/Components/AddWidgetModal/WidgetInputParams/WeatherCity/WeatherCity.js
+++ client/src/Components/AddWidgetModal/WidgetInputParams/WeatherCity/WeatherCity.js
@@ -1,40 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
 
 function WeatherCity({setParams, value}) {
     const [queryParams, setQueryParams] = useState(value !== undefined ? value : "");
     const [searchResult, setSearchResult] = useState([{label: "Paris"}]);
 
-    function searchCity() {
-        const options = {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Credentials": true
+    const searchCity = useCallback(
+        () => {
+            const options = {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Credentials": true
+                }
             }
-        }
-        const weatherURL = new URL('http://localhost:8080/weather/query');
-        weatherURL.searchParams.append("q", queryParams);
-
-        fetch(weatherURL, options)
-            .then(response => {
-                if (response.status === 200)
-                    return response.json();
-                throw new Error("failed to authenticate user")
-            })
-            .then(responseJSON => {
-                setSearchResult(responseJSON);
-            })
-            .catch(error => {
-                console.log('fetch error with wather', error);
-            })
-    }
+            const weatherURL = new URL('http://localhost:8080/weather/query');
+            weatherURL.searchParams.append("q", queryParams);
+    
+            fetch(weatherURL, options)
+                .then(response => {
+                    if (response.status === 200)
+                        return response.json();
+                    throw new Error("failed to authenticate user")
+                })
+                .then(responseJSON => {
+                    setSearchResult(responseJSON);
+                })
+                .catch(error => {
+                    console.log('fetch error with wather', error);
+                })
+        }, [queryParams]
+    );
 
     useEffect(() => {
         searchCity();
-    }, [queryParams]);
+    }, [searchCity]);
 
     return (
         <Autocomplete
