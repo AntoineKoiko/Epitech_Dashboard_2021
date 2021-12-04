7affeced262c63b83997cd7a518592866047582a
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-12-04 18:33:48 +0100

    fix(client/src/Components/RenderYoutubeSubNBWidgets.js) add useCallback

--- client/src/Components/Widgets/YoutubeSubNBWidget/RenderYoutubeSubNBWidget.js
+++ client/src/Components/Widgets/YoutubeSubNBWidget/RenderYoutubeSubNBWidget.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import YoutubeSubNBWidget from './YoutubeSubNBWidget';
 import { useState, useEffect } from 'react';
 
@@ -19,32 +19,34 @@ function RenderYoutubeSubNBWidget ({channelId, refresh, widgetData, setRefreshWi
         loading: true
     });
 
-    function fetchData() {
-        const ytURL = new URL('http://localhost:8080/youtube/channelStats');
-        ytURL.searchParams.append('channel_id', channelId);
+    const fetchData = useCallback(
+        () => {
+            const ytURL = new URL('http://localhost:8080/youtube/channelStats');
+            ytURL.searchParams.append('channel_id', channelId);
 
-        console.log('yt sub url ', ytURL.toString());
-        fetch(ytURL, requestOptions)
-            .then(response => {
-                if (response.status === 200)
-                    return response.json();
-                console.log('yt response : ', response)
-                throw new Error("failed to authenticate user")
-            })
-            .then(responseJSON => {
-                console.log('json yt response success ', responseJSON);
-                setChannelInfo(responseJSON);
-            })
-            .catch(error => {
-                console.log('fetch error for youtube sub nb');
-                console.log(error);
-            })
-    };
+            console.log('yt sub url ', ytURL.toString());
+            fetch(ytURL, requestOptions)
+                .then(response => {
+                    if (response.status === 200)
+                        return response.json();
+                    console.log('yt response : ', response)
+                    throw new Error("failed to authenticate user")
+                })
+                .then(responseJSON => {
+                    console.log('json yt response success ', responseJSON);
+                    setChannelInfo(responseJSON);
+                })
+                .catch(error => {
+                    console.log('fetch error for youtube sub nb');
+                    console.log(error);
+                })
+        }, [channelId]
+    );
 
     useEffect(() => {
         console.log(`initializing interval`);
         const interval = setInterval(() => {
-            console.log("youtube sub nb of ", channelName , "refresh is ", refresh, " mount at ", new Date().getSeconds() );
+            console.log("youtube sub nb of ", channelName , "refresh is ", refreshRate, " mount at ", new Date().getSeconds() );
             fetchData();
         }, refreshRate * 1000);
 
@@ -55,7 +57,7 @@ function RenderYoutubeSubNBWidget ({channelId, refresh, widgetData, setRefreshWi
             clearInterval(interval);
         };
 
-    }, [channelId]);
+    }, [fetchData, refreshRate, channelName]);
 
     return <YoutubeSubNBWidget channelName={channelName} channelInfo={channelInfo} widgetData={widgetData} setRefreshWidget={setRefreshWidget}/>
 }
