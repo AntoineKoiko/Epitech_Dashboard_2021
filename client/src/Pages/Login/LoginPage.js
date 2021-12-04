67ab141928edffb9e56cd860b74fcea8658a37b8
Aurélien Joncour <aurelien.joncour@epitech.eu>
2021-12-04 15:01:49 +0100

    feat(client/src/Pages/Login/LoginPage.js) add missing header in login

--- client/src/Pages/Login/LoginPage.js
+++ client/src/Pages/Login/LoginPage.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { createTheme } from '@mui/system';
-import LoginForm from '../../Components/Forms/LoginForm/LoginForm';
+import LoginForm from '../../Components/Forms/LoginForm/';
+import DefaultHeader from "../../Components/DefaultHeader/"
 import './LoginPage.css';
 import '../../Styles/GlobalStyle.css';
 
 function LoginPage() {
     return (
-        <div className="center-h main-container">
-            <p>Pour continuer, connectez-vous a Dashboard</p>
-            <Button sx={theme.inputField} className="input-field" href="http://localhost:8080/auth/spotify" variant="contained">Continuer avec spotifyd</Button>
-            <Button sx={theme.inputField} className="input-field" href="http://localhost:8080/auth/reddit" variant="contained">Continuer avec reddit</Button>
-            <Button sx={theme.inputField} className="input-field" href="http://localhost:8080/auth/google" variant="contained">Continuer avec google</Button>
-            <h2 className="word-divider"><span>ou</span></h2>
-            <LoginForm/>
+        <>
+            <DefaultHeader/>
+            <div className="center-h main-container">
+                <p>Pour continuer, connectez-vous a Dashboard</p>
+                <Button sx={theme.inputField} className="input-field" href="http://localhost:8080/auth/spotify" variant="contained">Continuer avec spotifyd</Button>
+                <Button sx={theme.inputField} className="input-field" href="http://localhost:8080/auth/reddit" variant="contained">Continuer avec reddit</Button>
+                <Button sx={theme.inputField} className="input-field" href="http://localhost:8080/auth/google" variant="contained">Continuer avec google</Button>
+                <h2 className="word-divider"><span>ou</span></h2>
+                <LoginForm/>
 
-            <div className="other-option-container">
-                <h2 className="word-divider"><span>Pas encore inscrit ?</span></h2>
-                <Button href="/register" sx={theme.inputField} variant="contained">
+                <div className="other-option-container">
+                    <h2 className="word-divider"><span>Pas encore inscrit ?</span></h2>
+                    <Button href="/register" sx={theme.inputField} variant="contained">
                 S'inscire
-                </Button>
+                    </Button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
